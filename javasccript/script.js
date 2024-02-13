c7516eaf1aff9b82657407c564f58ad3c9309bb9
Afrin <154747276+afrin823@users.noreply.github.com>
2024-02-13 15:03:50 +0600

    Add files via upload

---
+++ javasccript/script.js
@@ -0,0 +1,16 @@
+//  --- 1 number ---
+function htmlContent(){
+    document.getElementById('change').innerHTML = 'Sabikun Nahar Afrin';
+}
+//  --- 2 number ---
+function htmlAttribute(){
+    document.getElementById('light').src = 'img/pic_bulbon.gif';
+}
+//  --- 3 number ---
+function lightOff(){
+    document.getElementById('light').src = 'img/pic_bulboff.gif';
+}
+
+function fontColor(){
+    document.getElementById('style').style.color = 'red';
+}
